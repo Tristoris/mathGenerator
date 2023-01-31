5d9c86eecdf32057f8bdd78fe3e723ff1ac4d3ae
Alexander Yurkov <alexander.v.yurkov@gmail.com>
2023-01-31 21:07:59 +0100

    uwu

---
+++ EquationCreator.java
@@ -0,0 +1,17 @@
+
+/**
+ * Creates math equations
+ *
+ * @Alexander Yurkov
+ * @1.0
+ */
+public class EquationCreator
+{
+    // instance variables - replace the example below with your own
+    
+
+    public EquationCreator()
+    {
+        
+    }
+}
