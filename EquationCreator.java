1014c04e295b25fb52a7ed8df391a4a267cf5422
Alexander Yurkov <alexander.v.yurkov@gmail.com>
2023-01-31 21:22:53 +0100

    linearEqCreator

--- EquationCreator.java
+++ EquationCreator.java
@@ -9,9 +9,32 @@ import java.util.*;
 public class EquationCreator
 {
     // instance variables - replace the example below with your own
+    private String[] equationList;
 
-    public EquationCreator () {
+    public EquationCreator (int linearAmount, int quadraticAmount, int cubicAmount, int difficulty) {
+        int amount = linearAmount + quadraticAmount;
+        equationList = new String[amount];
+        for (int i = 0; i < equationList.length; i++) {
+            
+        }
+    }
+    
+    private String createLinear (int difficulty, double smallest, double biggest) {
+        String eq = "";
         
+        switch (difficulty) {
+            case 1:
+                int answer = randomNumber((int)smallest, (int)biggest);
+                int a = randomNumber(-20, 20);
+                
+                
+                
+                break;
+            default:
+                break;
+        }
+        
+        return eq;
     }
 
     private int randomNumber (int min, int max) {
